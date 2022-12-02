e681003c10524f4ab3270272cc75b033c5900a94
Deni Abdulaev <danny.abdulaev@bk.ru>
2022-12-02 21:22:48 +0300

    make_Lesson2ex3

---
+++ Lesson2Ex.3.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct adress
+{
+	string city;
+	string street;
+	int num_of_build;
+	int num_of_flat;
+	int zip_code;
+};
+
+void print_adress(adress x) {
+
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "Ваш адрес: " << x.city << ", улица " << x.street << "дом "
+		<< x.num_of_build << ", квартира " << x.num_of_flat << ", почтовый индекс " << x.zip_code;
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	adress your_adress;
+	std::cout << "Город: ";
+	std::getline(std::cin, your_adress.city);
+	
+	std::cout << "Улица: ";
+	std::getline(std::cin, your_adress.street);
+
+	std::cout << "Номер дома: ";
+	std::cin >> your_adress.num_of_build;
+
+	std::cout << "Номер квартиры: ";
+	std::cin >> your_adress.num_of_flat;
+
+	std::cout << "Индекс: ";
+	std::cin >> your_adress.zip_code;
+
+	print_adress(your_adress);
+}
